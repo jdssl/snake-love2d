51459734a026ef5e134116ab450d9221e4c84276
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-06-04 16:28:00 -0300

    feat: ensure snake limit

--- game.lua
+++ game.lua
@@ -3,12 +3,21 @@ local direction = { x = 0, y = 0 }
 
 local SIZE = 30
 local apple = { x = 0, y = 0 }
+local tail_length = 0
 
 Up = false
 Down = false
 Left = false
 Right = false
 
+local function tail_increment()
+  tail_length = tail_length + 1
+end
+
+local function new_apple_and_trail_incement()
+  Add_apple()
+  tail_increment()
+end
 function Add_apple()
   math.randomseed(os.time())
 
@@ -39,6 +48,23 @@ function Game_update()
 
   snake.x = snake.x + direction.x
   snake.y = snake.y + direction.y
+
+  local apple_captured = snake.x == apple.x and snake.y == apple.y
+
+  if apple_captured then
+    new_apple_and_trail_incement()
+  end
+
+  if snake.x < 0 then
+    snake.x = SIZE - 1
+  elseif snake.x > SIZE - 1 then
+    snake.x = 0
+  elseif snake.y < 0 then
+    snake.y = SIZE - 1
+  elseif snake.y > SIZE - 1 then
+    snake.y = 0
+  end
+
 end
 
 function Game_restart()
