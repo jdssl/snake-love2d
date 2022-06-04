7790524660a8189e7285e2ec62500b9febeb9dbc
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-06-04 17:05:47 -0300

    feat: add tail, text render

--- game.lua
+++ game.lua
@@ -4,6 +4,7 @@ local direction = { x = 0, y = 0 }
 local SIZE = 30
 local apple = { x = 0, y = 0 }
 local tail_length = 0
+local tail = {}
 
 Up = false
 Down = false
@@ -17,6 +18,7 @@ end
 local function new_apple_and_trail_incement()
   Add_apple()
   tail_increment()
+  table.insert(tail, {0, 0})
 end
 function Add_apple()
   math.randomseed(os.time())
@@ -26,13 +28,23 @@ function Add_apple()
 end
 
 function Game_draw()
-  -- snake
+  -- snake's head
   love.graphics.setColor(0.8, 0.9, 0.0, 1.0)
-  love.graphics.rectangle('fill', snake.x*SIZE, snake.y*SIZE, 30, 30, 10, 10)
+  love.graphics.rectangle('fill', snake.x*SIZE, snake.y*SIZE, SIZE, SIZE, 10, 10)
+
+  -- snake's tail
+  love.graphics.setColor(0.6, 0.9, 0.0, 1.0)
+  for _, value in ipairs(tail) do
+    love.graphics.rectangle('fill', value[1]*SIZE, value[2]*SIZE, SIZE, SIZE, 15, 15)
+  end
 
   -- apple
   love.graphics.setColor(1.0, 0.35, 0.4, 1.0)
-  love.graphics.rectangle('fill', apple.x*SIZE, apple.y*SIZE, 30, 30, 10, 10)
+  love.graphics.rectangle('fill', apple.x*SIZE, apple.y*SIZE, SIZE, SIZE, 15, 15)
+
+  -- text
+  love.graphics.setColor(1, 1, 1, 1)
+  love.graphics.print('collected apples: ' .. tail_length, 10, 10, 0, 1.5, 1.5, 0, 0, 0, 0)
 end
 
 function Game_update()
@@ -45,6 +57,8 @@ function Game_update()
   elseif Right then
     direction.x, direction.y = 1, 0
   end
+
+  local old = { x = snake.x, y = snake.y }
 
   snake.x = snake.x + direction.x
   snake.y = snake.y + direction.y
@@ -63,6 +77,14 @@ function Game_update()
     snake.y = SIZE - 1
   elseif snake.y > SIZE - 1 then
     snake.y = 0
+  end
+
+  if tail_length > 0 then
+    for _, value in ipairs(tail) do
+      local x, y = value[1], value[2]
+      value[1], value[2] = old.x, old.y
+      old.x, old.y = x, y
+    end
   end
 
 end
